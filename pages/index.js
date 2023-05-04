c8c38c5feadbc440820636d65c58d9a1e0f7aec5
Rane Gray <ranegray830@gmail.com>
2023-05-04 23:49:07 +0200

    added getServerSideProps to get all workouts from db

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Workouts from './workouts'
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export async function getServerSideProps() {
+  const workouts = await prisma.workout.findMany({
+    include: {
+      exercises: true
+    }
+  });
+
+  return {
+    props: {workouts: workouts},
+  };
+}
+
+export default function Home({workouts}) {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <Workouts />
+      <Workouts workouts={workouts} />
     </main>
   )
 }
